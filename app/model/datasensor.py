3145f7fae48d084de9987d7b4ba8f1999bb175ea
dhomanhuri <dhomanhuri@gmail.com>
2022-11-02 21:04:27 +0700

    add column telur

--- app/model/datasensor.py
+++ app/model/datasensor.py
@@ -5,6 +5,7 @@ class Datasensor(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     temp = db.Column(db.String(60), nullable=False)
     hum = db.Column(db.String(60), nullable=False)
+    telur = db.Column(db.String(60), nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
